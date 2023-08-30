eb8671025145aff40c701d20b27a271568eba577
Hasan <mr.k779@outlook.com>
2023-08-31 02:30:40 +0600

    Parser complete

--- parse.py
+++ parse.py
@@ -44,8 +44,6 @@ class Parser:
     # One of the following statements...
     # nl ::= '\n'+
     def nl(self):
-        print("NEWLINE")
-
         # Require at least one newline.
         self.match(TokenType.NEWLINE)
         # But we will allow extra newlines too, of course.
@@ -57,7 +55,6 @@ class Parser:
 
         # "PRINT" (expression | string)
         if self.check_token(TokenType.PRINT):
-            print("STATEMENT-PRINT")
             self.next_token()
 
             if self.check_token(TokenType.STRING):
@@ -100,7 +97,6 @@ class Parser:
             self.emitter.emit_line("}")
         elif self.check_token(TokenType.LABEL):
             # "LABEL" ident
-            print("STATEMENT-LABEL")
             self.next_token()
 
             # Make sure this label doesn't already exist.
@@ -112,7 +108,6 @@ class Parser:
             self.match(TokenType.IDENT)
         elif self.check_token(TokenType.GOTO):
             # "GOTO" ident
-            print("STATEMENT-GOTO")
             self.next_token()
 
             self.labels_gotoed.add(self.cur_token.text)
@@ -121,25 +116,35 @@ class Parser:
             self.match(TokenType.IDENT)
         elif self.check_token(TokenType.LET):
             # "LET" ident "=" expression
-            print("STATEMENT-LET")
             self.next_token()
 
             #  Check if ident exists in symbol table. If not, declare it.
             if self.cur_token.text not in self.symbols:
                 self.symbols.add(self.cur_token.text)
+                self.emitter.header_line("float " + self.cur_token.text + ";")
 
+            self.emitter.emit(self.cur_token.text + " = ")
             self.match(TokenType.IDENT)
             self.match(TokenType.EQ)
             self.expression()
+            self.emitter.emit_line(";")
         elif self.check_token(TokenType.INPUT):
             # "INPUT" ident
-            print("STATEMENT-INPUT")
             self.next_token()
 
             # If variable doesn't already exist, declare it.
             if self.cur_token.text not in self.symbols:
                 self.symbols.add(self.cur_token.text)
+                self.emitter.header_line("float " + self.cur_token.text + ";")
 
+            # Emit scanf but also validate the input. If invalid, set the variable to 0 and clear the input.
+            self.emitter.emit_line(
+                'if(0 == scanf("%' + 'f", &' + self.cur_token.text + ")) {"
+            )
+            self.emitter.emit_line(self.cur_token.text + " = 0;")
+            self.emitter.emit('scanf("%')
+            self.emitter.emit_line('*s");')
+            self.emitter.emit_line("}")
             self.match(TokenType.IDENT)
         else:
             # This is not a valid statement. Error!
@@ -177,12 +182,11 @@ class Parser:
 
     # comparison ::= expression (("==" | "!=" | ">" | ">=" | "<" | "<=") expression)+
     def comparison(self):
-        print("COMPARISON")
-
         self.expression()
 
         # Must be at least one comparison operator and another expression.
         if self.is_comparison_operator():
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
             self.expression()
         else:
@@ -190,45 +194,42 @@ class Parser:
 
         # Can have 0 or more comparison operator and expressions.
         while self.is_comparison_operator():
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
             self.expression()
 
     # expression ::= term {( "-" | "+" ) term}
     def expression(self):
-        print("EXPRESSION")
-
         self.term()
 
         # Can have 0 or more +/- and expressions.
         while self.check_token(TokenType.MINUS) or self.check_token(TokenType.PLUS):
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
             self.term()
 
     # term ::= unary {( "/" | "*" ) unary}
     def term(self):
-        print("TERM")
-
         self.unary()
 
         # Can have 0 or more *// and expressions.
         while self.check_token(TokenType.SLASH) or self.check_token(TokenType.ASTERISK):
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
             self.unary()
 
     # unary ::= ["+" | "-"] primary
     def unary(self):
-        print("UNARY")
-
         # Optional unary +/-
         if self.check_token(TokenType.PLUS) or self.check_token(TokenType.MINUS):
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
         self.primary()
 
     # primary ::= number | ident
     def primary(self):
-        print("PRIMARY (" + self.cur_token.text + ")")
-
         if self.check_token(TokenType.NUMBER):
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
         elif self.check_token(TokenType.IDENT):
             # Ensure the variable already exists.
@@ -237,6 +238,7 @@ class Parser:
                     "Referencing variable before assignment: " + self.cur_token.text
                 )
 
+            self.emitter.emit(self.cur_token.text)
             self.next_token()
         else:
             # Error!
