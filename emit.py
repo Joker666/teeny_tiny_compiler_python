e2c92d6284a41dfa7ff0de1dc9c7da963b44dbc2
Hasan <mr.k779@outlook.com>
2023-09-01 04:11:37 +0600

    Update emitter

--- emit.py
+++ emit.py
@@ -1,7 +1,7 @@
 # Emitter object keeps track of the generated code and outputs it.
 class Emitter:
     def __init__(self, full_path):
-        self.fullPath = full_path
+        self.full_path = full_path
         self.header = ""
         self.code = ""
 
@@ -15,5 +15,5 @@ class Emitter:
         self.header += code + "\n"
 
     def write_file(self):
-        with open(self.fullPath, "w") as outputFile:
+        with open(self.full_path, "w") as outputFile:
             outputFile.write(self.header + self.code)
